51b89d3bdac106953dafb6c92409cffe4c2cf995
Filip Wretenby <fiwr23@student.bth.se>
2024-04-09 16:12:51 +0200

    Session controller

---
+++ src/Controller/SessionController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SessionController extends AbstractController
+{
+    #[Route("/session", name: "view_session", methods: ['GET'])]
+    public function viewSession(
+        SessionInterface $session
+    ): Response {
+        $wholeSession = $session->all();
+
+
+        $data = [
+            "wholeSession" => $wholeSession
+        ];
+
+        return $this->render('session.html.twig', $data);
+    }
+
+    #[Route("/session/delete", name: "clear_session", methods: ['GET'])]
+    public function clearSession(
+        SessionInterface $session
+    ): Response {
+        $session->clear();
+        $this->addFlash(
+            'notice',
+            'Session has been deleted!'
+        );
+        return $this->redirectToRoute('view_session');
+    }
+
+}
