d195a060a78985d6675529c80551c630c83f77ab
Filip Wretenby <fiwr23@student.bth.se>
2024-04-22 16:57:59 +0200

    Slight code change

--- src/Card/CardHand.php
+++ src/Card/CardHand.php
@@ -1,6 +1,31 @@
-
 <?php
-/*
+
 namespace App\Card;
 
-class CardHand {} */
+use App\Card\CardGraphic;
+
+class CardHand
+{
+    /**
+    * @var (array<CardGraphic>)
+    */
+    protected $cardGraphicArray = [];
+
+    public function __construct()
+    {
+        $this->cardGraphicArray = [];
+    }
+
+    public function add(CardGraphic $cardGraphicIn): void
+    {
+        array_push($this->cardGraphicArray, $cardGraphicIn);
+    }
+
+    /**
+    * @return array<CardGraphic>
+    */
+    public function getAsArray(): array
+    {
+        return $this->cardGraphicArray;
+    }
+}
