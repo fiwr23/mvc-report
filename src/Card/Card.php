d5056affa410d081e47df42d3e57da367a28bcca
Filip Wretenby <fiwr23@student.bth.se>
2024-04-22 16:55:24 +0200

    Slight code change

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -4,6 +4,9 @@ namespace App\Card;
 
 class Card
 {
+    /**
+    * @var (string)
+    */
     protected $textValue;
 
     public function __construct(string $textIn)
@@ -11,19 +14,8 @@ class Card
         $this->textValue = $textIn;
     }
 
-    /*
-    public function roll(): int
-    {
-        $this->value = random_int(1, 6);
-        return $this->textValue;
-    }
 
-    public function getValue(): string
-    {
-        return $this->textValue;
-    }
-    */
-    public function getAsString(): string
+    public function getTextAsString(): string
     {
         return "{$this->textValue}";
     }
