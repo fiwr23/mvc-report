12a43651b3a9e20505fc3a1563baf3d3080bcd13
Filip Wretenby <fiwr23@student.bth.se>
2024-04-22 17:08:27 +0200

    Conform with code standard

--- src/Dice/DiceHand.php
+++ src/Dice/DiceHand.php
@@ -6,6 +6,9 @@ use App\Dice\Dice;
 
 class DiceHand
 {
+    /**
+    * @var array<Dice>
+    */
     private $hand = [];
 
     public function add(Dice $die): void
@@ -25,6 +28,9 @@ class DiceHand
         return count($this->hand);
     }
 
+    /**
+    * @return array<?int>
+    */
     public function getValues(): array
     {
         $values = [];
@@ -34,6 +40,9 @@ class DiceHand
         return $values;
     }
 
+    /**
+    * @return array<string>
+    */
     public function getString(): array
     {
         $values = [];
