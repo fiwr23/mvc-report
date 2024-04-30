eaf32106175b9e06306ee2bc4fed24029f10f791
Filip Wretenby <fiwr23@student.bth.se>
2024-04-30 22:38:11 +0200

    Added code blocks

--- src/Card/Card.php
+++ src/Card/Card.php
@@ -2,19 +2,26 @@
 
 namespace App\Card;
 
+/**
+ * Card class, holds card in text format.
+ */
 class Card
 {
     /**
     * @var (string)
     */
     protected $textValue;
-
+    /**
+     * Card constructor.
+    */
     public function __construct(string $textIn)
     {
         $this->textValue = $textIn;
     }
 
-
+    /**
+     * Returns card in text format.
+     */
     public function getTextAsString(): string
     {
         return "{$this->textValue}";
