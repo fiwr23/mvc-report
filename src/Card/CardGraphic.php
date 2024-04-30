f7d08b5de1a86ac62f6f5fab5d84e3a07f7baecd
Filip Wretenby <fiwr23@student.bth.se>
2024-04-30 22:34:57 +0200

    Added code blocks

--- src/Card/CardGraphic.php
+++ src/Card/CardGraphic.php
@@ -2,6 +2,9 @@
 
 namespace App\Card;
 
+/**
+ * CardGraphic class, holds card in graphic format.
+ */
 class CardGraphic extends Card
 {
     /**
@@ -9,12 +12,18 @@ class CardGraphic extends Card
     */
     protected $graphicValue;
 
+    /**
+     * Card constructor.
+     */
     public function __construct(string $textIn, string $graphicIn)
     {
         parent::__construct($textIn);
         $this->graphicValue = $graphicIn;
     }
 
+    /**
+     * Returns card as'graphic'.
+     */
     public function getAsString(): string
     {
         return $this->graphicValue;
