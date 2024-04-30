fefa6c0db71a8a337f94825c9588a5b07bd074bd
Filip Wretenby <fiwr23@student.bth.se>
2024-04-30 22:28:59 +0200

    Added code blocks

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -9,6 +9,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Controller for session page.
+ */
 class SessionController extends AbstractController
 {
     #[Route("/session", name: "view_session", methods: ['GET'])]
