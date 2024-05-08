570d8ac7bb2bce9a900fbfc6e2a9e91db286f7ff
Filip Wretenby <fiwr23@student.bth.se>
2024-05-08 16:59:15 +0200

    Initial commit

---
+++ src/Controller/MetricsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Controller for metrics page.
+ */
+class MetricsController extends AbstractController
+{
+    #[Route("/metrics", name: "metrics")]
+    public function metrics(): Response
+    {
+
+        return $this->render('metrics.html.twig');
+    }
+}
