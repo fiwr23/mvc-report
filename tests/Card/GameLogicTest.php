2646e7613780b768f8decb9059b6cf3686f22216
Filip Wretenby <fiwr23@student.bth.se>
2024-05-08 21:28:39 +0200

    Split into two

--- tests/Card/GameLogicTest.php
+++ tests/Card/GameLogicTest.php
@@ -20,46 +20,6 @@ class GameLogicTest extends TestCase
     }
 
     /**
-     * Get data for new game.
-     */
-    public function testNewGame(): void
-    {
-        $gameLogic = new GameLogic();
-        $res = $gameLogic->newGame();
-        $this->assertIsArray($res);
-        $this->assertIsArray($res['current_deck']);
-        $this->assertEquals('player', $res['current_player']);
-        $this->assertInstanceOf("\App\Card\CardHand", $res['bank_hand']);
-        $this->assertInstanceOf("\App\Card\CardHand", $res['player_hand']);
-        $this->assertEquals("-", $res['last_player_card']);
-        $this->assertEquals("-", $res['last_bank_card']);
-        $this->assertEquals([0], $res['player_hand_score']);
-        $this->assertEquals([0], $res['bank_hand_score']);
-        $this->assertEquals(0, $res['bank_wins']);
-        $this->assertEquals(0, $res['player_wins']);
-        $this->assertEquals("ongoing", $res['game_state']);
-    }
-
-    /**
-     * Get data for next round.
-     */
-    public function testNextRound(): void
-    {
-        $gameLogic = new GameLogic();
-        $res = $gameLogic->nextRound();
-
-        $this->assertIsArray($res);
-        $this->assertEquals('player', $res['current_player']);
-        $this->assertInstanceOf("\App\Card\CardHand", $res['bank_hand']);
-        $this->assertInstanceOf("\App\Card\CardHand", $res['player_hand']);
-        $this->assertEquals("-", $res['last_player_card']);
-        $this->assertEquals("-", $res['last_bank_card']);
-        $this->assertEquals([0], $res['player_hand_score']);
-        $this->assertEquals([0], $res['bank_hand_score']);
-        $this->assertEquals("ongoing", $res['game_state']);
-    }
-
-    /**
      * Construct deckofcards and get all cards shuffled.
      */
     public function testGetAllCardsShuffled(): void
@@ -142,22 +102,5 @@ class GameLogicTest extends TestCase
         $res = $gameLogic->sumCurrentHand($cardHand);
 
         $this->assertEquals([1, 14], $res);
-    }
-
-    /**
-     * Converts array of int to string with ' or ' between
-     */
-    public function testscoreString(): void
-    {
-        $gameLogic = new GameLogic();
-        $intArr = [1, 10];
-        $res = $gameLogic->scoreString($intArr);
-
-        $this->assertEquals("1 or 10", $res);
-
-        $intArr = [13];
-        $res = $gameLogic->scoreString($intArr);
-
-        $this->assertEquals("13", $res);
     }
 }
