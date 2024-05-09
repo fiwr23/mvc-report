20951afde88eb16f21a8bcd052b084f82d47c326
Filip Wretenby <fiwr23@student.bth.se>
2024-05-09 13:41:06 +0200

    From cardjoncontrollertwo

---
+++ src/Card/CheckNumCards.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Card;
+
+use App\Card\CardGraphic;
+
+/**
+ * Check cards in deck for drawNumJson, returns data array.
+ */
+class CheckNumCards
+{
+    /**
+     * Returns data array.
+     * @param array<null|CardGraphic> $shuffledDeck
+     * @return array<string,array<string>|int>|array<string>
+    */
+    public function check(int $num, array $shuffledDeck): array
+    {
+        switch ($num > count($shuffledDeck)) {
+            case true:
+                return [
+                    'warning' => 'Too few cards in deck! Reset by clicking on card/deck/shuffle or delete session'
+                ];
+            default:
+
+                $drawnCards = [];
+                for ($x = 0; $x < $num; $x++) {
+                    array_push($drawnCards, array_pop($shuffledDeck));
+                }
+
+                $cardsToSend = [];
+                if (is_array($drawnCards)) {
+                    /** @var CardGraphic $x*/
+                    foreach ($drawnCards as $x) {
+                        array_push($cardsToSend, $x->getAsString());
+                    }
+                }
+                /** @var int $tmpCount*/
+                $tmpCount = count($shuffledDeck);
+
+                /** @var array<string> $cardsToSend*/
+                return [
+                    "cardsLeft" => $tmpCount,
+                    "cards" => $cardsToSend
+                ];
+        }
+    }
+}
