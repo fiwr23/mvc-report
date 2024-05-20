76625e2f5be1e437ecb50808bb8f9db1dc32420b
Filip Wretenby <fiwr23@student.bth.se>
2024-05-20 22:11:53 +0200

    Small change

--- src/Card/GameLogic.php
+++ src/Card/GameLogic.php
@@ -98,7 +98,7 @@ class GameLogic
             'Ace' => 1, 'Two' => 2, 'Three' => 3, 'Four' => 4,
             'Five' => 5, 'Six' => 6, 'Seven' => 7,
             'Eight' => 8, 'Nine' => 9, 'Ten' => 10,
-            'Knight' => 11, 'Queen' => 12,
+            'Jack' => 11, 'Queen' => 12,
             'King' => 13];
 
         $cardHandasArray = $cardHandIn->getAsArray();
